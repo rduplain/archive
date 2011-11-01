e160b4d21718517502a0a161be3d89361930d552
Ron DuPlain <ron.duplain@gmail.com>
2011-10-31 23:17:40 -0400

    Add a couple quick comments.

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST' and request.is_xhr:
         app.logger.debug('request.form: ' + repr(request.form))
+        # See success callback in index.html.
         return jsonify(inputs=request.form.keys())
     return render_template('index.html')
 
