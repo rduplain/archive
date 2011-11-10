8bf00141e70049bfb743c774eb4183d64b0df096
Ron DuPlain <ron.duplain@gmail.com>
2011-11-09 21:04:17 -0500

    Wrap full demo of change/button triggers.

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Just a 5-minute test with the `examples/` in
 https://github.com/nervetattoo/jquery-autosave
 
 Install Flask in a Python virtualenv with `pip install flask`.
+
+Run development server with `python app.py` and visit http://localhost:5000/.
--- app.py
+++ app.py
@@ -9,7 +9,8 @@ def index():
     if request.method == 'POST' and request.is_xhr:
         app.logger.debug('request.form: ' + repr(request.form))
         # See success callback in index.html.
-        return jsonify(inputs=request.form.keys())
+        # Return the form as-is to provide a round-trip demo.
+        return jsonify(**request.form)
     return render_template('index.html')
 
 
