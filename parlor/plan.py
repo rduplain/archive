08e0d163698244561e93d49bc311534e75870482
Ron DuPlain <ron.duplain@gmail.com>
2014-08-31 10:09:32 -0400

    Add APIs for plans to make suggestions to parents.

--- parlor/plan.py
+++ parlor/plan.py
@@ -16,6 +16,7 @@ class Plan(object):
         self.before_first_request_lock = Lock()
         self.after_request_fns = []
         self.errorhandlers = {}
+        self.suggestions = []
         self.provider_data = {}
 
     def __iter__(self):
@@ -36,6 +37,7 @@ class Plan(object):
             plan.before_first_request_fns + self.before_first_request_fns
         self.after_request_fns.extend(plan.after_request_fns)
         self.errorhandlers.update(plan.errorhandlers)
+        self.suggestions.extend(plan.suggestions)
         self.provider_data.update(plan.provider_data)
 
     def copy(self):
@@ -50,6 +52,7 @@ class Plan(object):
             copy.copy(self.before_first_request_fns)
         plan.after_request_fns = copy.copy(self.after_request_fns)
         plan.errorhandlers = copy.copy(self.errorhandlers)
+        plan.suggestions = copy.copy(self.suggestions)
         plan.provider_data = copy.copy(self.provider_data)
         return plan
 
@@ -87,10 +90,19 @@ class Plan(object):
             endpoint = fn.__name__
         self.routes[(rule, tuple(methods))] = endpoint, fn
 
+    def suggest_add_url_rule(self, *a, **kw):
+        self.suggest('add_url_rule', *a, **kw)
+
     def route(self, rule, **keywords):
         """Decorate callable as a route, passes keywords to `add_url_rule`."""
         def decorator(fn):
             self.add_url_rule(rule, fn=fn, **keywords)
+            return fn
+        return decorator
+
+    def suggest_route(self, rule, **keywords):
+        def decorator(fn):
+            self.suggest('add_url_rule', rule, fn=fn, **keywords)
             return fn
         return decorator
 
@@ -112,8 +124,16 @@ class Plan(object):
         self.before_request_fns.append(fn)
         return fn
 
+    def suggest_before_request(self, fn):
+        self.suggest('before_request', fn)
+        return fn
+
     def before_first_request(self, fn):
         self.before_first_request_fns.append(fn)
+        return fn
+
+    def suggest_before_first_request(self, fn):
+        self.suggest('before_first_request', fn)
         return fn
 
     def iter_before_request_handlers(self):
@@ -128,6 +148,10 @@ class Plan(object):
 
     def after_request(self, fn):
         self.after_request_fns.append(fn)
+        return fn
+
+    def suggest_after_request(self, fn):
+        self.suggest('after_request', fn)
         return fn
 
     def iter_after_request_handlers(self):
@@ -145,6 +169,15 @@ class Plan(object):
                 return x
             return decorator
 
+    def suggest_provider(self, note, provider, name=False):
+        if provider is not None:
+            self.suggest('provider', note, provider=provider, name=name)
+        else:
+            def decorator(x):
+                self.suggest('provider', note, provider=x, name=name)
+                return x
+            return decorator
+
     def factory(self, note, fn=None):
         if fn is not None:
             self._register_provider_data('factory', note, fn=fn)
@@ -154,8 +187,20 @@ class Plan(object):
                 return f
             return decorator
 
+    def suggest_factory(self, note, fn=None):
+        if fn is not None:
+            self.suggest('factory', note, fn=fn)
+        else:
+            def decorator(f):
+                self.suggest('factory', note, fn=f)
+                return f
+            return decorator
+
     def value(self, note, scalar):
         self._register_provider_data('value', note, scalar=scalar)
+
+    def suggest_value(self, note, scalar):
+        self.suggest('value', note, scalar)
 
     def _register_provider_data(self, recipe_name, note, **recipe_keywords):
         self.provider_data[note] = recipe_name, recipe_keywords
@@ -163,3 +208,43 @@ class Plan(object):
     def iter_provider_data(self):
         for note, (recipe_name, recipe_keywords) in self.provider_data.items():
             yield recipe_name, note, recipe_keywords
+
+    def suggest(self, recipe_name, *args, **kwargs):
+        self.suggestions.append(Suggestion(recipe_name, *args, **kwargs))
+
+    def iter_suggestions(self):
+        for suggestion in self.suggestions:
+            yield suggestion
+
+    def take_suggestions(self, logger=None):
+        if logger is None:
+            log = lambda msg: None
+        else:
+            log = lambda msg: logger.info(msg)
+
+        for plan in iter(self):
+            for suggestion in plan.iter_suggestions():
+                log('Taking suggestion: {!r}'.format(suggestion))
+                suggestion(self)
+
+
+class Suggestion(object):
+    def __init__(self, name, *args, **kwargs):
+        self.name = name
+        self.args = args
+        self.kwargs = kwargs
+
+    def __call__(self, plan):
+        method = getattr(plan, self.name)
+        return method(*self.args, **self.kwargs)
+
+    @staticmethod
+    def format(*args, **kwargs):
+        arg_sig = ', '.join(repr(arg) for arg in args)
+        kwarg_sig = ', '.join('{}={}'.format(str(k), repr(v))
+                              for k, v in kwargs.items())
+        return ', '.join([x for x in (arg_sig, kwarg_sig) if x])
+
+    def __repr__(self):
+        arguments = self.format(*self.args, **self.kwargs)
+        return 'plan.{}({})'.format(self.name, arguments)
